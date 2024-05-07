0831cc6ce7d21d34c1b7cb87e3e26ff6b078732d
muhammadffa <muhammaddaffa463@gmail.com>
2024-05-07 21:08:53 +0700

    books and authors

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express()
+
+const port = process.env.PORT || 7777
+
+app.use(express.json());
+
+app.get("/ping", (_, res) => {
+    res.status(200).json({message : "server running well"})
+})
+
+app.listen(port , () => {
+    console.log(`Server run on port : ${port}`);
+    
+})
